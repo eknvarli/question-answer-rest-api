7ab91ccde39e0acaffd0a29f11a9c6d0396a0829
Ekin Ilter Varli <ekinnos@tutanota.com>
2024-03-02 00:13:05 +0300

    Create a test user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,23 @@
-const signup = (req, res, next) => {
+const User = require('../models/User');
+
+const signup = async (req, res, next) => {
+    // POST DATA
+    const name = 'Volkan Konak';
+    const email = 'info@volkankonak.com';
+    const password = '123456';
+
+    // async & await
+    const user = await User.create({
+        name: name,
+        email: email,
+        password: password
+    });
+
     res
     .status(200)
     .json({
-        success: true
+        success: true,
+        data: user
     })
 };
 
